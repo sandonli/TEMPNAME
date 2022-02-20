ed1f64dd8534d783f819a2bc1a173f8bd9131ff9
sandonli <sandonli@outlook.com>
2022-02-20 07:17:15 -0800

    maybe works

--- webpage.js
+++ webpage.js
@@ -1,5 +1,5 @@
 // import axios from "axios";
-const axios = require("./axios");
+import axios from "../axios";
 const YOUR_API_KEY = "AIzaSyCUVPsRkh4YHvTIzdoh2f1EGrSlR24V_JE";
 
 let script = document.createElement("script");
