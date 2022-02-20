8c1a1fd134559df170775dd66cd85244e8c3422c
sandonli <sandonli@outlook.com>
2022-02-19 19:35:07 -0800

    made changes and added files

--- webpage.js
+++ webpage.js
@@ -1,11 +1,54 @@
-let directionsService;
-
-function initMap() {
-    let options = {
-        zoom: 8,
-        center: {lat: 41.3601, lng: -71.0589}
-    }
-
-    let map = new google.maps.Map(document.getElementById("map"), options);
-    let marker = new google.maps.Marker({position: options.center, map:map});
+function emptyDOM(elem) {
+    while (elem.firstChild) elem.removeChild(elem.firstChild);
 }
+
+function createDOM(htmlString) {
+    let template = document.createElement("template");
+    template.innerHTML = htmlString.trim();
+    return template.content.firstChild;
+}
+class WelcomeDisplay {
+    constructor() {
+        let self = this;
+        //this.elem = createDOM(/*add html here*/"");
+        this.elem = document.getElementById("app-view");
+        console.log(this.elem);
+        this.inputTime = this.elem.querySelector("input[name=time]"); //REMEMBER TO CHANGE
+        this.inputBike = this.elem.querySelector("input[name=bike]"); //REMEMBER TO CHANGE
+        this.inputCar = this.elem.querySelector("input[name=car]"); //REMEMBER TO CHANGE
+        this.searchButton = this.elem.querySelector("input[name=searchButton]");
+        
+        this.searchButton.addEventListener("click", function() {
+            let bikeChecked = false;
+            let carChecked = false;
+            let time;
+            if (self.inputBike.checked) {
+                bikeChecked = true;
+            }
+            if (self.inputCar.checked) {
+                carChecked = true;
+            }
+            if (self.inputTime.value.trim() == "" || isNaN(self.inputTime.value) || parseInt(self.inputTime.value) <= 0) {
+                alert("INVALID DATA ENTRY");
+            }
+            time = parseInt(self.inputTime.value);
+            self.inputTime.value = "";
+        })
+    }
+    
+}
+class LoadingScreen {
+
+}
+
+class MapDisplay {
+    constructor() {
+        this.elem = createDOM(/*add html here*/);
+    }
+}
+
+function main() {
+    let welcome = new WelcomeDisplay();
+}
+
+window.addEventListener("load", main, false);
