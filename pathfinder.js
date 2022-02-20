9730f53bd3f10e94915cb24f0e15c022f98eef16
Jason <lijason.li@mail.utoronto.ca>
2022-02-20 01:09:05 -0500

    added nodejs dependecies

--- pathfinder.js
+++ pathfinder.js
@@ -1,10 +1,19 @@
-var axios = require('axios');
+const axios = require('axios');
+var  YOUR_API_KEY = "AIzaSyCUVPsRkh4YHvTIzdoh2f1EGrSlR24V_JE";
+
+var start = "Disneyland";
+var dest = "Universal Studios Hollywood";
+
+start = start.replaceAll(" ", "+");
+dest = dest.replaceAll(" ", "+");
 
 var config = {
   method: 'get',
-  url: 'https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCUVPsRkh4YHvTIzdoh2f1EGrSlR24V_JE',
+  url: "https://maps.googleapis.com/maps/api/directions/json?origin=" + start + "&destination=" + dest  + "&key=" + YOUR_API_KEY,
   headers: { }
 };
+
+console.log(config);
 
 axios(config)
 .then(function (response) {
